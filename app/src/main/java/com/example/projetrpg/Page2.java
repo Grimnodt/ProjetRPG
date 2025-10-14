604a92c5391c65fb862152c3d449c127e7b58e01
Gregoire24 <devoucouxgregoire@gmail.com>
2025-10-14 14:50:45 +0200

    Sélection unique Q3

--- app/src/main/java/com/example/projetrpg/Page2.java
+++ app/src/main/java/com/example/projetrpg/Page2.java
@@ -17,7 +17,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Page2 extends AppCompatActivity {
-
+    CheckBox cb1, cb2, cb3, cb4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +30,30 @@ public class Page2 extends AppCompatActivity {
         });
 
         Intent intent = getIntent();
-        int resultQ1 = intent.getIntExtra("question 1", -1);
-        int resultQ2 = intent.getIntExtra("question 2", -1);
+        int resultQ3 = intent.getIntExtra("question 3", -1);
+        int resultQ4 = intent.getIntExtra("question 4", -1);
+
+        CheckBox cb1 = findViewById(R.id.cb1Q3);
+        CheckBox cb2 = findViewById(R.id.cb2Q3);
+        CheckBox cb3 = findViewById(R.id.cb3Q3);
+        CheckBox cb4 = findViewById(R.id.cb4Q3);
+
+        View.OnClickListener listener = v -> {
+            cb1.setChecked(v == cb1);
+            cb2.setChecked(v == cb2);
+            cb3.setChecked(v == cb3);
+            cb4.setChecked(v == cb4);
+        };
+
+        cb1.setOnClickListener(listener);
+        cb2.setOnClickListener(listener);
+        cb3.setOnClickListener(listener);
+        cb4.setOnClickListener(listener);
     }
 
     public void onValiderClick(View view) {
+        RadioGroup group3 = findViewById(R.id.radioGroup3);
+        RadioGroup group4 = findViewById(R.id.radioGroup4);
         CheckBox check1Q3 = findViewById(R.id.cb1Q3);
         CheckBox check2Q3 = findViewById(R.id.cb2Q3);
         CheckBox check3Q3 = findViewById(R.id.cb3Q3);
@@ -52,13 +71,21 @@ public class Page2 extends AppCompatActivity {
             return;
         }
 
-        ArrayList<Integer> resultQ3 = new ArrayList<>();
-        if (check1Q3.isChecked()) resultQ3.add(1);
-        if (check2Q3.isChecked()) resultQ3.add(2);
-        if (check3Q3.isChecked()) resultQ3.add(3);
-        if (check4Q3.isChecked()) resultQ3.add(4);
+        int resultQ3 = group3.getCheckedRadioButtonId();
+        int resultQ4 = group4.getCheckedRadioButtonId();
 
-        ArrayList<Integer> resultQ4 = new ArrayList<>();
+        if (group3.getCheckedRadioButtonId() == -1 || group4.getCheckedRadioButtonId() == -1){
+            invalid.setText("Une question n'a pas été répondu");
+            return;
+        }
+
+        ArrayList<Integer> resultQ03 = new ArrayList<>();
+        if (cb1.isChecked()) resultQ3.add(1);
+        if (cb2.isChecked()) resultQ3.add(2);
+        if (cb3.isChecked()) resultQ3.add(3);
+        if (cb4.isChecked()) resultQ3.add(4);
+
+        ArrayList<Integer> resultQ04 = new ArrayList<>();
         if (check1Q4.isChecked()) resultQ3.add(1);
         if (check2Q4.isChecked()) resultQ3.add(2);
         if (check3Q4.isChecked()) resultQ3.add(3);
