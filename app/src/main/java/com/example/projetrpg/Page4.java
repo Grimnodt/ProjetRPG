661e661830a358479a61f0d3f7e0bdf908de66fc
Kael LISSARAGUE <KaelLissarrague@outlook.com>
2025-10-29 20:25:08 +0100

    p4

--- app/src/main/java/com/example/projetrpg/Page4.java
+++ app/src/main/java/com/example/projetrpg/Page4.java
@@ -1,4 +1,48 @@
 package com.example.projetrpg;
 
-public class Page4 {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Page4 extends AppCompatActivity {
+
+    private TextView invalid = findViewById(R.id.tx_Invalid3);
+
+    private ImageButton tanjiro_red = findViewById(R.id.imageButton5);
+    private ImageButton tanjiro_blue = findViewById(R.id.imageButton6);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_page3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onValiderClick(View view) {
+
+        if (!tanjiro_red.isPressed() || !tanjiro_blue.isPressed()) {
+            invalid.setText("Veuillez cocher que le formulaire vous plaît. Merci. ");
+            return;
+        }
+
+        invalid.setText("Félicitations, vous avez fini le questionnaire, " +
+                "nous allons maintenant déterminer votre personnage.");
+
+        Intent intent = new Intent(this, Page5.class);
+    }
 }
