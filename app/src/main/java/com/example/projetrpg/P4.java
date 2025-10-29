878e9ef1cbc9230a366be341722b54e9d601d4e0
Kael LISSARAGUE <KaelLissarrague@outlook.com>
2025-10-29 21:41:45 +0100

    P4

--- app/src/main/java/com/example/projetrpg/P4.java
+++ app/src/main/java/com/example/projetrpg/P4.java
@@ -1,11 +1,7 @@
 package com.example.projetrpg;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Page4 extends AppCompatActivity {
+public class P4 extends AppCompatActivity {
 
     private TextView invalid;
 
@@ -22,13 +18,13 @@ public class Page4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_page3);
+        setContentView(R.layout.activity_p4);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        invalid = findViewById(R.id.tx_Invalid3);
+        invalid = findViewById(R.id.tx_Invalid4);
     }
 
     public void onValiderClick(View view) {
@@ -41,7 +37,7 @@ public class Page4 extends AppCompatActivity {
         invalid.setText("Félicitations, vous avez fini le questionnaire, " +
                 "nous allons maintenant déterminer votre personnage.");
 
-        Intent intent = new Intent(this, Page5.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, Page5.class);
+        //startActivity(intent);
     }
 }
--- app/src/main/java/com/example/projetrpg/Page5.java
+++
@@ -1,4 +0,0 @@
-package com.example.projetrpg;
-
-public class Page5 {
-}
