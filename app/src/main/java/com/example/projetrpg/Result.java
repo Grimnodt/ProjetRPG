9f39ca0f0019661e31a418a2fabfb6a2bbfe488a
Kael LISSARAGUE <KaelLissarrague@outlook.com>
2025-10-30 21:31:02 +0100

    final

--- app/src/main/java/com/example/projetrpg/Result.java
+++ app/src/main/java/com/example/projetrpg/Result.java
@@ -2,7 +2,8 @@ package com.example.projetrpg;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View; // Import nécessaire
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ public class Result extends AppCompatActivity {
 
     private ReponsesQuiz reponses;
     private TextView tvResultatFinal;
+    private ImageView ivResultatImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,36 +30,104 @@ public class Result extends AppCompatActivity {
         });
 
         tvResultatFinal = findViewById(R.id.tv_resultat_final);
+        ivResultatImage = findViewById(R.id.imageView);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             reponses = getIntent().getParcelableExtra(ReponsesQuiz.KEY, ReponsesQuiz.class);
         } else {
-            reponses = getIntent().getParcelableExtra(ReponsesQuiz.KEY); // LE ELSE EST DEPRECATED, ON EST SUR API 36 DONC C'EST NORMAL, ON RENTRE PAS DANS LE ELSE QUAND CA MARCHE.
+            reponses = getIntent().getParcelableExtra(ReponsesQuiz.KEY);
         }
 
         if (reponses != null) {
             String personnage = analyserReponses(reponses);
             tvResultatFinal.setText("Vous êtes...\n" + personnage + " !");
+            afficherImagePersonnage(personnage);
         } else {
             tvResultatFinal.setText("Erreur: Impossible de charger les résultats.");
         }
     }
 
     private String analyserReponses(ReponsesQuiz r) {
+        if (r.getReponseQ11() == R.id.rb_q11_rep2) {
+            return "V (Cyberpunk)";
+        }
+
+        if (r.getReponseQ3() == R.id.rb_q3_rep2) {
+            return "Flash";
+        }
 
         if (r.getReponseQ8() == R.id.rb_q8_rep5) {
             return "Muzan Kibutsuji";
+        }
+
+        if (r.getReponseQ10() == R.id.rb_q10_rep4 && r.getReponseQ2() == R.id.rb_q2_rep3) {
+            return "Griffith";
+        }
+
+        if (r.getReponseQ10() == R.id.rb_q10_rep4 && r.getReponseQ2() == R.id.rb_q2_rep1) {
+            return "Sauron";
+        }
+
+        if (r.getReponseQ10() == R.id.rb_q10_rep4) {
+            return "Joker";
         }
 
         if (r.getReponseQ2() == R.id.rb_q2_rep1 && r.getReponseQ10() == R.id.rb_q10_rep1) {
             return "Batman";
         }
 
-        if (r.getReponseQ1() == R.id.rb_q1_rep2) {
-            return "Un Fonceur !";
+        if (r.getReponseQ1() == R.id.rb_q1_rep2 && r.getReponseQ7() == R.id.rb_q7_rep2) {
+            return "Kratos";
         }
 
-        return "Un aventurier";
+        if (r.getReponseQ7() == R.id.rb_q7_rep3 && r.getReponseQ2() == R.id.rb_q2_rep1) {
+            return "Paul Atreides";
+        }
+
+        if (r.getReponseQ1() == R.id.rb_q1_rep1 && r.getReponseQ2() == R.id.rb_q2_rep2) {
+            return "Pippin";
+        }
+        return "Un Aventurier (Paul Atreides)";
+    }
+
+    private void afficherImagePersonnage(String personnage) {
+        switch (personnage) {
+            case "Batman":
+                ivResultatImage.setImageResource(R.drawable.batman);
+                break;
+            case "Flash":
+                ivResultatImage.setImageResource(R.drawable.flash);
+                break;
+            case "Griffith":
+                ivResultatImage.setImageResource(R.drawable.griffith);
+                break;
+            case "Joker":
+                ivResultatImage.setImageResource(R.drawable.joker);
+                break;
+            case "Kratos":
+                ivResultatImage.setImageResource(R.drawable.kratos);
+                break;
+            case "Muzan Kibutsuji":
+                ivResultatImage.setImageResource(R.drawable.muzan_jackson);
+                break;
+            case "Paul Atreides":
+            case "Un Aventurier (Paul Atreides)":
+                ivResultatImage.setImageResource(R.drawable.paul);
+                break;
+            case "Pippin":
+                ivResultatImage.setImageResource(R.drawable.pinpin);
+                break;
+            case "Sauron":
+                ivResultatImage.setImageResource(R.drawable.sauron);
+                break;
+            case "V (Cyberpunk)":
+                ivResultatImage.setImageResource(R.drawable.cyberpunk);
+                break;
+            default:
+                        // ON CACHE L'IMAGE SI Y A UN SOUCI.
+                ivResultatImage.setVisibility(View.GONE);
+                break;
+        }
     }
 
     public void onQuitterClick(View view) {
